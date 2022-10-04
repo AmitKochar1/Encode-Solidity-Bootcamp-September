4a3c798506dde5381cdceeb2c7af13f4f7a537f0
Sol-Devloper <blockhunt14@gmail.com>
2022-10-04 23:55:31 +1300

    Update in contract - Lesson 9

---
+++ Lesson_9/test/Test.ts
@@ -0,0 +1,73 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("NFT Shop", () => {
+    beforeEach(() => { });
+
+    // describe("When the Shop contract is deployed", () => {
+    //     it("defines the ratio as provided in parameters", () => {
+    //         throw new Error("Not implemented");
+    //     });
+
+    //     it("uses a valid ERC20 as payment token", () => {
+    //         throw new Error("Not implemented");
+    //     });
+    // });
+
+    // describe("When a user purchase an ERC20 from the Token contract", () => {
+    //     it("charges the correct amount of ETH", () => {
+    //         throw new Error("Not implemented");
+    //     });
+
+    //     it("gives the correct amount of tokens", () => {
+    //         throw new Error("Not implemented");
+    //     }); 
+    // });
+
+    // describe("When a user burns an ERC20 at the Token contract", () => {
+    //     it("gives the correct amount of ETH", () => {
+    //         throw new Error("Not implemented");
+    //     });
+
+    //     it("burns the correct amount of tokens", () => {
+    //         throw new Error("Not implemented");
+    //     });
+    // });
+
+    // describe("When a user purchase a NFT from the Shop contract", () => {
+    //     it("charges the correct amount of ETH", () => {
+    //         throw new Error("Not implemented");
+    //     });
+
+    //     it("updates the owner account correctly", () => {
+    //         throw new Error("Not implemented");
+    //     });
+
+    //     it("update the pool account correctly", () => {
+    //         throw new Error("Not implemented");
+    //     });
+
+    //     it("favors the pool with the rounding", () => {
+    //         throw new Error("Not implemented");
+    //     });
+    // });
+
+    // describe("When a user burns their NFT at the Shop contract", () => {
+    //     it("gives the correct amount of ERC20 tokens", () => {
+    //         throw new Error("Not implemented");
+    //     });
+    //     it("updates the pool correctly", () => {
+    //         throw new Error("Not implemented");
+    //     });
+    // });
+
+    // describe("When the owner withdraw from the Shop contract", () => {
+    //     it("recovers the right amount of ERC20 tokens", () => {
+    //         throw new Error("Not implemented");
+    //     });
+
+    //     it("updates the owner account correctly", () => {
+    //         throw new Error("Not implemented");
+    //     });
+    // });
+});
